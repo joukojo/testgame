63e821a61075b1d8af752e4f7c958797af699f78
Jouko Johansson <jouko.johansson@sanoma.fi>
2012-10-25 23:43:07 +0300

    Refactored The code

--- src/main/java/com/github/joukojo/testgame/CollisionDetectionWorker.java
+++ src/main/java/com/github/joukojo/testgame/CollisionDetectionWorker.java
@@ -14,42 +14,19 @@ public class CollisionDetectionWorker implements Runnable {
 
 	private final static Logger LOG = LoggerFactory
 			.getLogger(CollisionDetectionWorker.class);
+	private boolean isRunning;
 
 	@Override
 	public void run() {
 		WorldCore worldCore = WorldCoreFactory.getWorld();
-		while (true) {
-			List<Moveable> bullets = worldCore.getMoveableObjects("bullets");
+		isRunning = true;
+		while (isRunning) {
+			final List<Moveable> bullets = worldCore.getMoveableObjects("bullets");
 
 			if (bullets != null && !bullets.isEmpty()) {
-				for (Moveable moveableBullet : bullets) {
-					Bullet bullet = (Bullet) moveableBullet;
-					List<Moveable> monsters = worldCore
-							.getMoveableObjects("monsters");
-					if (monsters != null && !monsters.isEmpty()) {
-						for (Moveable moveableMonster : monsters) {
-							Monster monster = (Monster) moveableMonster;
-							// FIXME do the location calculation better
-							if (monster != null && bullet != null) {
-								if (!monster.isDestroyed() && !bullet.isDestroyed()) {
-									int deltaX = Math.abs(bullet.locationX
-											- monster.locationX);
-									int deltaY = Math.abs(bullet.locationY
-											- monster.locationY);
-									LOG.trace("deltaX {}", deltaX);
-									LOG.trace("deltaY {}", deltaY);
-									if (deltaX < 20 && deltaY < 20) {
-										LOG.debug("we have a hit");
-										monster.setDestroyed(true);
-										bullet.setDestroyed(true);
-										
-										Player player  = (Player) worldCore.getMoveable("player");
-										player.score += 100; 
-									}
-								}
-							}
-						}
-					}
+				for (final Moveable moveableBullet : bullets) {
+					final Bullet bullet = (Bullet) moveableBullet;
+					isBulletInCollisionWithMonster(worldCore, bullet);
 				}
 			}
 
@@ -58,6 +35,39 @@ public class CollisionDetectionWorker implements Runnable {
 			Thread.yield();
 		}
 
+	}
+
+	private void isBulletInCollisionWithMonster(WorldCore worldCore, Bullet bullet) {
+		List<Moveable> monsters = worldCore
+				.getMoveableObjects("monsters");
+		if (monsters != null && !monsters.isEmpty()) {
+			for (Moveable moveableMonster : monsters) {
+				Monster monster = (Monster) moveableMonster;
+				// FIXME do the location calculation better
+				if (monster != null && bullet != null) {
+					isBulletAndMonsterInCollision(worldCore, bullet, monster);
+				}
+			}
+		}
+	}
+
+	private void isBulletAndMonsterInCollision(WorldCore worldCore, Bullet bullet, Monster monster) {
+		if (!monster.isDestroyed() && !bullet.isDestroyed()) {
+			int deltaX = Math.abs(bullet.locationX
+					- monster.locationX);
+			int deltaY = Math.abs(bullet.locationY
+					- monster.locationY);
+			LOG.trace("deltaX {}", deltaX);
+			LOG.trace("deltaY {}", deltaY);
+			if (deltaX < 20 && deltaY < 20) {
+				LOG.debug("we have a hit");
+				monster.setDestroyed(true);
+				bullet.setDestroyed(true);
+				
+				Player player  = (Player) worldCore.getMoveable("player");
+				player.score += 100; 
+			}
+		}
 	}
 
 }
