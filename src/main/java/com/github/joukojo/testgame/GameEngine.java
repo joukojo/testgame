26245be51197dbd8eaf87da5c8af13783bb56e77
Jouko Johansson <joukojo@gmail.com>
2012-11-02 15:57:07 +0200

    the new game engine for handling threads

---
+++ src/main/java/com/github/joukojo/testgame/GameEngine.java
@@ -0,0 +1,75 @@
+package com.github.joukojo.testgame;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.joukojo.testgame.world.core.WorldCore;
+import com.github.joukojo.testgame.world.core.WorldCoreFactory;
+import com.github.joukojo.testgame.world.core.WorldCoreTask;
+import com.github.joukojo.testgame.world.graphics.GraphicEngine;
+import com.github.joukojo.testgame.world.graphics.GraphicEngineWorker;
+
+public class GameEngine {
+	private final static Logger LOG = LoggerFactory.getLogger(GameEngine.class);
+	private GraphicEngine engine;
+
+	private MonsterCreatorTask monsterCreatorTask;
+	private CollisionDetectionWorker collisionDetector;
+	private GraphicEngineWorker graphicEngineWorker;
+	private WorldCoreTask worldCoreTask;
+	
+	private ExecutorService executorService;
+	private static GameEngine INSTANCE; 
+	
+	
+	public void init() {
+		engine = new GraphicEngine();
+
+		WorldCore worldCore = WorldCoreFactory.getWorld();		
+		Player player = new Player();
+		player.positionX = 500; 
+		player.positionY = 100; 
+
+		worldCore.addMoveable("player", player);
+		monsterCreatorTask = new MonsterCreatorTask();
+
+		collisionDetector = new CollisionDetectionWorker(); 
+
+		graphicEngineWorker = new GraphicEngineWorker(engine);
+
+		worldCoreTask = new WorldCoreTask();
+		
+		executorService = Executors.newCachedThreadPool();
+		
+	}
+	
+	public void startGame() {
+		LOG.debug("starting up the game engine");
+		executorService.execute(monsterCreatorTask);
+		executorService.execute(collisionDetector);
+		executorService.execute(graphicEngineWorker);
+		executorService.execute(worldCoreTask);
+		
+		
+		
+	}
+	
+	public void stopGame() {
+		
+		
+	}
+
+	public static synchronized GameEngine getInstance() {
+		
+		if( INSTANCE == null ) {
+			INSTANCE = new GameEngine();
+		}
+		
+		return INSTANCE; 
+	}
+	
+	
+}
