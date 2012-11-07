a3fd5c02bc27eb6f6eeac2e4c452629249db1b16
Jouko Johansson <joukojo@gmail.com>
2012-11-07 16:18:27 +0200

    Fixed imports

--- src/main/java/com/github/joukojo/testgame/App.java
+++ src/main/java/com/github/joukojo/testgame/App.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.joukojo.testgame.world.core.Moveable;
 import com.github.joukojo.testgame.world.core.WorldCore;
 import com.github.joukojo.testgame.world.core.WorldCoreFactory;
 
