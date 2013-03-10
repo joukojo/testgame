b24c6a432a051b80ec0441cc504c4f1a11f91a18
Jouko Johansson <joukojo@gmail.com>
2013-03-11 00:00:37 +0200

    fixed pmds

--- src/main/java/com/github/joukojo/testgame/world/core/WorldCoreFactory.java
+++ src/main/java/com/github/joukojo/testgame/world/core/WorldCoreFactory.java
@@ -3,10 +3,14 @@ package com.github.joukojo.testgame.world.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WorldCoreFactory {
+public final class WorldCoreFactory {
 	private final static Logger LOG = LoggerFactory
 			.getLogger(WorldCoreFactory.class);
 
+	
+	private WorldCoreFactory() {
+		// no instantiate 
+	}
 	private static WorldCore instance;
 
 	public static WorldCore getWorld() {
